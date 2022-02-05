1fa6622fbb5d666b4adffc1f074d94291b507d09
CrisC3 <Cris@CANALES.HOME>
2022-02-04 21:52:35 -0600

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -7,65 +7,66 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [tracesData, setTracesData] = useState([]);
   const [loadedData, setLoadedData] = useState(false);
-  const [countProc, setCountProc] = useState(0);
 
   async function tracesFile(event) {
     const allFiles = event.target.files;
     let filesContainer = "";
 
-    if (allFiles.length >= 4) {
-      alert("Please upload up to 3 files at a time");
-      return;
-    } else {
-      setIsLoading(true);
+    setIsLoading(true);
 
-      for await (const file of allFiles) {
-        const reader = new FileReader();
-        reader.readAsText(file);
-        const result = await new Promise((resolve, reject) => {
-          reader.onloadend = function (event) {
-            filesContainer += reader.result;
-            resolve();
-          };
-        });
-      }
-      xmlParser(filesContainer);
-      setIsLoading(false);
+    for await (const file of allFiles) {
+      const reader = new FileReader();
+      reader.readAsText(file);
+      const result = await new Promise((resolve, reject) => {
+        reader.onloadend = function (event) {
+          filesContainer += reader.result;
+          resolve();
+        };
+      });
     }
+    xmlParser(filesContainer);
+    setIsLoading(false);
   }
 
   function xmlParser(xmlText) {
-    const XMLParser = require("react-xml-parser");
-    const xmlData = "<root>" + xmlText + "</root>";
-    const xml = new XMLParser().parseFromString(xmlData); // Assume xmlText contains the example XML
-    const traceEvents = xml.getElementsByTagName("E2ETraceEvent");
+    const xmlData = new DOMParser().parseFromString(
+      "<root>" + xmlText + "</root>",
+      "text/xml"
+    );
+
     setIsLoading(false);
-    traceObjects(traceEvents);
+    traceObjects(xmlData);
   }
 
   async function traceObjects(traces) {
-    // let processedCount = 0;
+    const E2ETraceEvents = await traces.getElementsByTagName("E2ETraceEvent");
 
-    await traces.forEach((trace) => {
-      const timeCreated =
-        trace.getElementsByTagName("TimeCreated")[0].attributes.SystemTime;
-      const serverName = trace.getElementsByTagName("ServerName")[0].value;
-      const userName = trace.getElementsByTagName("UserName")[0].value;
-      const userIpAddress = trace.getElementsByTagName("UserIP")[0].value;
+    for await (const trace of E2ETraceEvents) {
+      const xmlString = new XMLSerializer().serializeToString(trace);
+      const systemTime = trace
+        .getElementsByTagName("TimeCreated")[0]
+        .getAttribute("SystemTime");
+      const serverName =
+        trace.getElementsByTagName("ServerName")[0].textContent;
+      const userName = trace.getElementsByTagName("UserName")[0].textContent;
+      const userIpAddress = trace.getElementsByTagName("UserIP")[0].textContent;
       const componentName =
-        trace.getElementsByTagName("ComponentName")[0].value;
+        trace.getElementsByTagName("ComponentName")[0].textContent;
       const applicationName =
-        trace.getElementsByTagName("ApplicationName")[0].value;
-      const traceLevel = trace.getElementsByTagName("TraceLevel")[0].value;
-      const message = trace.getElementsByTagName("Message")[0].value;
-      const subMessage = trace.getElementsByTagName("SubMessage")[0].value;
-      const stackTrace = trace.getElementsByTagName("StackTrace")[0].value;
+        trace.getElementsByTagName("ApplicationName")[0].textContent;
+      const traceLevel =
+        trace.getElementsByTagName("TraceLevel")[0].textContent;
+      const message = trace.getElementsByTagName("Message")[0].textContent;
+      const subMessage =
+        trace.getElementsByTagName("SubMessage")[0].textContent;
+      const stackTrace =
+        trace.getElementsByTagName("StackTrace")[0].textContent;
 
       setTracesData((prevData) => [
         ...prevData,
         {
           id: ++tracesData.length,
-          TimeCreated: timeCreated,
+          SystemTime: systemTime,
           ServerName: serverName,
           Username: userName,
           UserIP: userIpAddress,
@@ -75,12 +76,10 @@ function App() {
           Message: message,
           SubMessage: subMessage,
           StackTrace: stackTrace,
+          FullTrace: xmlString,
         },
       ]);
-
-      setCountProc((prevCountProc) => 1 + prevCountProc);
-    });
-
+    }
     setIsLoading(false);
     setLoadedData(true);
   }
@@ -90,14 +89,18 @@ function App() {
       <h1>SiteCom Web Traces Viewer</h1>
       <input type="file" accept=".svclog" onChange={tracesFile} multiple />
       <div>
-        {" "}
-        <p>{/*{isLoading ? "Processing..." + countProc : null}*/}</p>{" "}
+        {isLoading ? (
+          <>
+            <p>{isLoading && "Processing..."}</p>
+            <DisplayLoading color={"gray"} type={"spin"} />
+          </>
+        ) : loadedData ? (
+          <>
+            <hr />
+            <SiteComDataGrid tracesData={tracesData} />
+          </>
+        ) : null}
       </div>
-      {isLoading ? (
-        <DisplayLoading color={"gray"} type={"spin"} />
-      ) : loadedData ? (
-        <SiteComDataGrid tracesData={tracesData} />
-      ) : null}
     </div>
   );
 }
